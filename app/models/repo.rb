796462e1c801c97b371f31335834023933feacc1
Erinna Chen <erinna.chen@gmail.com>
2016-04-05 11:40:31 -0600

    Add user can view repositories

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,12 +1,17 @@
 class Repo
-  attr_reader :name
+  attr_reader :name, :updated_at, :description, :star_count, :fork_count, :language
   def initialize(data)
-    @name = data.name
+    @name                 = data.name
+    @language             = data.language
+    @fork_count           = data.forks_count
+    @star_count           = data.stargazers_count
+    @description          = data.description
+    @fork                 = data.fork
+    @updated_at           = DateTime.parse(data.updated_at)
   end
 
   def self.parse_from_data(response)
     data = JSON.parse(response.body, object_class: OpenStruct)
-    binding.pry
     data.map do |repo_data|
       Repo.new(repo_data)
     end
