de084848dfb703280fa224ab97f4fc4a76fff6f8
Erinna Chen <erinna.chen@gmail.com>
2016-04-04 19:01:12 -0600

    Add user can logout and flash messages

--- spec/features/user_authorized_via_oauth_spec.rb
+++ spec/features/user_authorized_via_oauth_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "user logs in with oauth" do
     stub_omniauth
     visit "/"
     click_link "Sign in with Github"
-    save_and_open_page
+
     expect(current_path).to eq "/"
     expect(page).to have_css('img[alt="@worace"]')
     expect(page).to have_css('img[src="https://avatars1.githubusercontent.com/u/1227440"]')
---
+++ spec/features/user_log_out_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.feature "user logsout" do
+  include SpecHelpers
+  scenario "logs in and sees avatar and dropdown" do
+    stub_omniauth
+    visit "/"
+    click_link "Sign in with Github"
+    expect(page).to have_content("Signed in as worace")
+
+    click_link "Sign out"
+
+    expect(current_path).to eq "/"
+    save_and_open_page
+    expect(page).to_not have_css('img[alt="@worace"]')
+    expect(page).to_not have_css('img[src="https://avatars1.githubusercontent.com/u/1227440"]')
+    expect(page).to_not have_content("Signed in as worace")
+    expect(page).to have_content("Successfully signed out")
+    expect(page).to have_content("Sign in with Github")
+  end
+end
