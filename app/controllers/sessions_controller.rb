f6c054a6183277e396bf271793a9a0230bf47cd7
Erinna Chen <erinna.chen@gmail.com>
2016-04-04 18:42:10 -0600

    Add user login and sees their avatar on navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < ApplicationController
   def create
     if user = User.from_omniauth(request.env["omniauth.auth"])
       session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as #{user.nickname}"
     end
-    redirect_to "/hello"
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/:username", to: "users#show"
   get "/auth/github", as: "github_login"
   get "/auth/github/callback", to: "sessions#create"
+  get "/", to: "users#show", as: "root"
+  delete "/logout", to: "sessions#destroy", as: "logout"
 end
