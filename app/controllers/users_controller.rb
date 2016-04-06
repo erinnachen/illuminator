6b8676720f903bbb1198afd9efd7b4f5be02d95e
Erinna Chen <erinna.chen@gmail.com>
2016-04-06 13:21:13 -0600

    Add user can get starred repositories

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def show
     @github_user = GithubUser.find(params[:username])
-    @orgs = @github_user.orgs
+    # @orgs = @github_user.orgs
     @repos = @github_user.repos
+  end
+
+  def create
+    #send an API call
   end
 end
--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -2,13 +2,24 @@ class UserService
   attr_reader :connection
 
   def initialize
-    @connection = Faraday.new(url: "https://api.github.com/users")
+    @connection = Faraday.new(url: "https://api.github.com")
     @auth = {client_id: ENV['github_client_id'],
              client_secret: ENV['github_client_secret']}
   end
 
-  def get(path)
-    @connection.get(path, @auth)
+  def get(path, page = nil)
+    params = @auth
+    params[:page] = page if page
+    @connection.get(path, params)
+  end
+
+  def get_starred(path, page = nil)
+    @connection.get do |conn|
+      conn.url path
+      conn.headers["Accept"] = "application/vnd.github.v3.star+json"
+      conn.params = @auth
+      conn.params["page"] = page if page
+    end
   end
 
   def user(username)
@@ -16,15 +27,45 @@ class UserService
   end
 
   def repos(username)
-    parse_response(get("/users/#{username}/repos"))
+    repos = []
+    response = get("/users/#{username}/repos")
+    repos << parse_response(response)
+    if get_more_pages?(response.headers)
+      (2..max_page(response.headers[:link])).each do |n|
+        response = get("/users/#{username}/repos", n)
+        repos << parse_response(response)
+      end
+    end
+    repos.flatten
   end
 
   def orgs(username)
     parse_response(get("/users/#{username}/orgs"))
   end
 
+  def starred(username)
+    starred = []
+    response = get_starred("/users/#{username}/starred")
+    starred << parse_response(response)
+    if get_more_pages?(response.headers)
+      (2..max_page(response.headers[:link])).each do |n|
+        response = get_starred("/users/#{username}/starred", n)
+        starred << parse_response(response)
+      end
+    end
+    starred.flatten
+  end
+
   private
     def parse_response(response)
       JSON.parse(response.body, object_class: OpenStruct)
+    end
+
+    def get_more_pages?(headers)
+      !!headers[:link]
+    end
+
+    def max_page(link_header)
+      link_header.split(/;|,/)[-2][-2].to_i
     end
 end
