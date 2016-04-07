293a5af9b280cd54e16371e7557987f30a84eff4
Erinna Chen <erinna.chen@gmail.com>
2016-04-07 09:24:10 -0600

    Add model tests for Org and Repo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,6 @@
 class UsersController < ApplicationController
   def show
     @github_user = GithubUser.find(params[:username])
-    # if starred?
-    #   @repos = @github_user.starred
-    # else
-    #   @repos = @github_user.repos
-    # end
   end
 
-
-  private
-    def starred?
-      params[:tab] == "starred" || request.fullpath.include?("/starred")
-    end
 end
